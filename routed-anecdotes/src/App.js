f6270295ce4bcf9c222c1872453940b24c31205c
mdboticano <mdboticano@gmail.com>
2019-09-17 09:50:25 -0700

    7.2: individual anecdotes can be viewed by their id

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -21,10 +21,22 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id} >
+        <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+      </li>)}
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdote }) => {
+  return (
+    <div>
+      <h2>{anecdote.content}</h2>
+      <p>has {anecdote.votes} votes</p>
+      <p>for more info, see <a href={anecdote.info}>{anecdote.info}</a></p>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -140,6 +152,9 @@ const App = () => {
         <Route exact path="/about" render={() => {
           return  <About />
         }} />
+        <Route exact path="/anecdotes/:id" render={({ match }) => 
+          <Anecdote anecdote={anecdoteById(match.params.id)} />
+        } />
       </Router>
       <Footer />
     </div>
