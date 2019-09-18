9083c7b03feb927a6275bf440f86e4c760056f52
mdboticano <mdboticano@gmail.com>
2019-09-18 09:26:40 -0700

    Removed old commented code from CreateBlog

--- bloglist/frontend/src/components/CreateBlog.js
+++ bloglist/frontend/src/components/CreateBlog.js
@@ -1,26 +1,20 @@
 import React from 'react'
 
-// const CreateBlog = ({ addBlog, author, handleAuthor, title, handleTitle,
-//   url, handleUrl }) => {
-
-  const CreateBlog = ({ addBlog, author, title, url}) => {
+const CreateBlog = ({ addBlog, author, title, url }) => {
 
   return (
     <form id="create-blog" onSubmit={addBlog}>
       <h2>Add blog</h2>
       <div className="blog-input">
         <label htmlFor="title">Title</label>
-        {/* <input value={title} onChange={handleTitle} /> */}
         <input {...title} />
       </div>
       <div className="blog-input">
         <label htmlFor="author">Author</label>
-        {/* <input value={author} onChange={handleAuthor} /> */}
         <input {...author} />
       </div>
       <div className="blog-input">
         <label htmlFor="url">URL</label>
-        {/* <input value={url} onChange={handleUrl} /> */}
         <input {...url} />
       </div>
       <button type='submit'>Add</button>
