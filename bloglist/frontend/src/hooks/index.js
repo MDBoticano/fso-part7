b98bf9f6301adb53846d17b21acf904d90713f65
mdboticano <mdboticano@gmail.com>
2019-09-18 09:25:55 -0700

    Added deleteEntry to useResource hook to fix issue where delete wouldn't work

--- bloglist/frontend/src/hooks/index.js
+++ bloglist/frontend/src/hooks/index.js
@@ -28,25 +28,36 @@ export const useResource = (baseUrl) => {
   const setToken = (newToken) => {
     token = `bearer ${newToken}`
   }
-  
+
   const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
   }
-  
+
   const create = newObject => {
     const config = {
       headers: { Authorization: token }
     }
-  
+
     const request = axios.post(baseUrl, newObject, config)
     return request.then(response => response.data)
   }
-  
+
   const update = (id, newObject) => {
     const request = axios.put(`${baseUrl}/${id}`, newObject)
     return request.then(response => response.data)
   }
 
-  return [ value , { getAll, create, update, setToken, setValue } ]
+  const deleteEntry = (id) => {
+    const config = {
+      headers: { Authorization: token }
+    }
+    const request = axios.delete(`${baseUrl}/${id}`, config)
+
+    return request.then(() => {
+      return getAll()
+    })
+  }
+
+  return [ value , { getAll, create, update, deleteEntry, setToken, setValue } ]
 }
